46f7021c24f3cc9e8188b2ff85a8ede6a031e410
Szymon <szymonsadowski3@gmail.com>
2018-04-12 23:04:50 +0100

    - regex to dfa service

--- services/greenery-service.py
+++ services/greenery-service.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import json
 from greenery import fsm, lego
 
 app = Flask(__name__)
@@ -33,6 +32,28 @@ def dfa_to_regex():
     return jsonify({
         "regex": str(lego.from_fsm(constructed_automata))
     })
+
+
+@app.route('/regex/to/dfa', methods=['POST'])
+def regex_to_dfa():
+    received_json = request.get_json(silent=True)
+
+    received_regex = received_json['regex']
+    constructed_regex = lego.parse(received_regex)
+    constructed_fsm = constructed_regex.to_fsm()
+
+    alphabet = list(constructed_fsm.alphabet)
+    prepared_alphabet = [letter for letter in alphabet if not isinstance(letter, fsm.anything_else_cls)]
+
+    response = {
+        "alphabet": prepared_alphabet,
+        "states": list(constructed_fsm.states),
+        "finals": list(constructed_fsm.finals),
+        "initial": str(constructed_fsm.initial),
+        "map": constructed_fsm.map,
+    }
+
+    return jsonify(response)
 
 
 if __name__ == '__main__':
